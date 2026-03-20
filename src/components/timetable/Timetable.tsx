849691ae7c3442dc058201f5ebfbc107ee8e1860
Andrei25i <andreiiosif2504@gmail.com>
2026-03-20 15:05:47 +0200

    IST-57: Build schedule grid layout

---
+++ src/components/timetable/Timetable.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import type { ScheduleEvent } from "@/types/ScheduleEvent.types";
+import { DAYS, TIME_SLOTS } from "@/constants/timetable.constants";
+import styles from "./Timetable.module.css";
+
+interface TimetableProps {
+  events?: ScheduleEvent[];
+}
+
+const Timetable = ({ events = [] }: TimetableProps) => {
+  return (
+    <div className={styles.timetableContainer}>
+      <div className={styles.timetableGrid}>
+        <div className={styles.headerCell}></div>
+        {DAYS.map((day) => (
+          <div key={day} className={styles.dayHeader}>
+            {day}
+          </div>
+        ))}
+
+        {TIME_SLOTS.map((time) => (
+          <React.Fragment key={time}>
+            <div className={styles.timeLabel}>
+              <span>{time.split(" - ")[0]}</span>
+              <span className={styles.timeDivider}>|</span>
+              <span>{time.split(" - ")[1]}</span>
+            </div>
+
+            {DAYS.map((day) => {
+              const currentEvent = events.find(
+                (e) => e.day === day && e.timeSlot === time,
+              );
+
+              return (
+                <div key={`${day}-${time}`} className={styles.gridCell}>
+                  {currentEvent ? "EVENT" : null}
+                </div>
+              );
+            })}
+          </React.Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Timetable;

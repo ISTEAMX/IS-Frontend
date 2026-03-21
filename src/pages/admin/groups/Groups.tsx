af05b0eaf18b325aff3bb3722fac14d285dc1306
Antonescu Andrei-Iosif <78093165+Andrei25i@users.noreply.github.com>
2026-03-21 12:37:10 +0200

    IST-86: Build admin dashboard layout (#14)

---
+++ src/pages/admin/groups/Groups.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "@/components/ui/PageHeader";
+
+const Groups = () => {
+  return (
+    <>
+      <PageHeader title="Grupe" onAddClick={() => {}} />
+    </>
+  );
+};
+
+export default Groups;
---
+++ src/pages/admin/teachers/Teachers.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "@/components/ui/PageHeader";
+
+const Teachers = () => {
+  return (
+    <>
+      <PageHeader title="Profesori" onAddClick={() => {}} />
+    </>
+  );
+};
+
+export default Teachers;

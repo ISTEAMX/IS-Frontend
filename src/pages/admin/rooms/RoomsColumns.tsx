7de25b39bc83d71658d12fc48d5bc6379b101380
Antonescu Andrei-Iosif <78093165+Andrei25i@users.noreply.github.com>
2026-03-23 10:22:26 +0200

    IST-87: Build data table component (#15)

---
+++ src/pages/admin/rooms/RoomsColumns.tsx
@@ -0,0 +1,60 @@
+import type { Room } from "@/types/Room.types";
+import type { ColumnDef } from "@tanstack/react-table";
+import styles from "./RoomsColumns.module.css";
+import { BiEdit, BiTrash } from "react-icons/bi";
+
+export const columns: ColumnDef<Room>[] = [
+  {
+    accessorKey: "name",
+    header: "Denumire Sală",
+    cell: ({ row }) => (
+      <div>
+        <div className={styles.roomName}>{row.original.name}</div>
+        <div className={styles.roomLocation}>{row.original.location}</div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "type",
+    header: "Tip",
+    cell: ({ row }) => {
+      const roomType = row.original.type.toLowerCase();
+
+      return (
+        <span className={`${styles.roomTypeBadge} ${styles[roomType] || ""}`}>
+          {row.original.type}
+        </span>
+      );
+    },
+  },
+  {
+    accessorKey: "capacity",
+    header: "Capacitate",
+    cell: ({ row }) => (
+      <span
+        className={styles.roomCapacity}
+      >{`${row.original.capacity} locuri`}</span>
+    ),
+  },
+  {
+    id: "actions",
+    header: "Acțiuni",
+    enableSorting: false,
+    cell: ({ row }) => (
+      <div className={styles.actionsWrapper}>
+        <button
+          className={`${styles.actionBtn} ${styles.editBtn}`}
+          onClick={() => console.log("Edit:", row.original.id)}
+        >
+          <BiEdit className={styles.icon} />
+        </button>
+        <button
+          className={`${styles.actionBtn} ${styles.deleteBtn}`}
+          onClick={() => console.log("Delete:", row.original.id)}
+        >
+          <BiTrash className={styles.icon} />
+        </button>
+      </div>
+    ),
+  },
+];
---
+++ src/types/Room.types.ts
@@ -0,0 +1,7 @@
+export type Room = {
+  id: string;
+  name: string;
+  location: string;
+  type: string;
+  capacity: number;
+};

2cac938dd69b5fe4495a057cca7d72a7f6e1adf9
Antonescu Andrei-Iosif <78093165+Andrei25i@users.noreply.github.com>
2026-04-04 17:30:54 +0300

    IST-63: Implement filters UI (#40)

---
+++ src/components/ui/ScheduleFilters.tsx
@@ -0,0 +1,43 @@
+import useGroups from "@/hooks/useGroups";
+import useRooms from "@/hooks/useRooms";
+import useTeachers from "@/hooks/useTeachers";
+import styles from "./Filters.module.css";
+import FilterSelect from "./FilterSelect";
+
+const ScheduleFilters = () => {
+  const { rooms } = useRooms();
+  const { teachers } = useTeachers();
+  const { groups } = useGroups();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.filtersBar}>
+        <FilterSelect
+          label="Grupă"
+          options={groups}
+          getLabel={(g) => g.identifier}
+          getValue={(g) => g.id}
+          placeHolder="Toate grupele"
+        />
+
+        <FilterSelect
+          label="Profesor"
+          options={teachers}
+          getLabel={(t) => `${t.firstName} ${t.lastName}`}
+          getValue={(t) => t.id}
+          placeHolder="Toți profesorii"
+        />
+
+        <FilterSelect
+          label="Sală"
+          options={rooms}
+          getLabel={(r) => r.name}
+          getValue={(r) => r.id}
+          placeHolder="Toate sălile"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ScheduleFilters;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,16 @@
 import Timetable from "@/components/timetable/Timetable";
+import ScheduleFilters from "@/components/ui/ScheduleFilters";
 import { MOCK_EVENTS } from "@/mocks/schedule.events";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <>
-      <Timetable events={MOCK_EVENTS} />
+      <ScheduleFilters />
+
+      <div className={styles.contentWrapper}>
+        <Timetable events={MOCK_EVENTS} />
+      </div>
     </>
   );
 };

e68bd5e89c87b2e910b0ed407b0f6503626ecca9
Andrei25i <andreiiosif2504@gmail.com>
2026-04-04 17:22:11 +0300

    IST-63: Implement filters UI

---
+++ src/components/ui/FilterSelect.tsx
@@ -0,0 +1,35 @@
+import styles from "./Filters.module.css";
+
+interface FilterSelectProps<T> {
+  label: string;
+  options: T[];
+  getLabel: (item: T) => string;
+  getValue: (item: T) => string | number;
+  placeHolder: string;
+  value?: string | number;
+}
+
+const FilterSelect = <T,>({
+  label,
+  options,
+  getLabel,
+  getValue,
+  placeHolder,
+  value,
+}: FilterSelectProps<T>) => {
+  return (
+    <div className={styles.filterField}>
+      <label className={styles.filterLabel}>{label}</label>
+      <select className={styles.select} value={value || ""} onChange={() => {}}>
+        <option value={""}>{placeHolder}</option>
+        {options.map((item) => (
+          <option key={getValue(item)} value={getValue(item)}>
+            {getLabel(item)}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default FilterSelect;

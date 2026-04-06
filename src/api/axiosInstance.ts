a947a35576471318f66f45d981f056969d151839
zoczeli <czelizoltandragos@gmail.com>
2026-04-06 20:29:59 +0300

    IST-127: Fix deployment url in aws

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -6,7 +6,7 @@ export interface CustomConfig extends InternalAxiosRequestConfig {
 }
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 api.interceptors.request.use((config: CustomConfig) => {
